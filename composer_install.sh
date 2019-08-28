02d3f515de78343b0e01ae7b3d495b6badcbd470
hungfhq <hungfhq@gmail.com>
2019-08-28 23:48:30 +0700

    add all

---
+++ composer_install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#COLORS
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+
+# Update
+echo -e "$Cyan \n Update $Color_Off"
+sudo apt update
+
+# Install prerequisites
+echo -e "$Cyan \n Install prerequisites $Color_Off"
+sudo apt install curl php-cli php-mbstring git unzip
+
+# Install Composer
+echo -e "$Cyan \n Installing Composer $Color_Off"
+cd ~
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+php composer.phar
